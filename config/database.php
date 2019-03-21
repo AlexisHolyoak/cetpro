927e37da4d93216e80bbe40efd5e4da83537b300
AlexisHolyoak <peraltaholyoak.aj@gmail.com>
2019-03-21 12:11:52 -0500

    database config

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'cetpro'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_5d11da2f5160c64'),
+            'username' => env('DB_USERNAME', 'bb7b90966b0786'),
+            'password' => env('DB_PASSWORD', '547e287b'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
